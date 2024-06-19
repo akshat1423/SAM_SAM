2365abf320e225d6f77e52bd76f3bc7a7f093c02
Kaushik035 <kaushikmandal629@gmail.com>
2024-06-19 17:00:04 +0530

    trouble shoot all the error on merging Khushi's an Kaushik's code

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import FakeHeader from "./components/FakeHeader/FakeHeader";
 import About from "./components/About/About";
 import Carousel from "./components/Homee/Carousel";
 
+import Cities from "./components/Cities/Cities";
+import Testimonials from "./components/Testimonials/Testimonials";
+import Sponsors from "./components/Sponsors/Sponsors";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
@@ -12,11 +16,33 @@ function App() {
       <Navbarr></Navbarr>
       <Carousel></Carousel>
 
-      <hr />
-      <div id="about" style={{width:'100vw', height:'10vh',backgroundColor: '#2d2c31'}}></div>
+      <div
+        id="about"
+        style={{ width: "100vw", height: "10vh", backgroundColor: "#2d2c31" }}
+      ></div>
       <About></About>
-      <hr />
+
+      <div
+        id="cities"
+        style={{ width: "100vw", height: "10vh", backgroundColor: "#2d2c31" }}
+      ></div>
+
+      <Cities></Cities>
+      <div
+        id="testimonials"
+        style={{ width: "100vw", height: "10vh", backgroundColor: "#2d2c31" }}
+      ></div>
+
+      <Testimonials></Testimonials>
+      <div
+        id="sponsors"
+        style={{ width: "100vw", height: "10vh", backgroundColor: "#2d2c31" }}
+      ></div>
+
+      <Sponsors></Sponsors>
       <Images></Images>
+
+      <Footer></Footer>
     </div>
   );
 }
