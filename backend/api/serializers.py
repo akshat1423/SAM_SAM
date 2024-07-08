d56040f737340afe664c0909544f36e9d8dbc9aa
Kaushik035 <kaushikmandal629@gmail.com>
2024-07-08 18:50:07 +0530

    deleted unnecessary things

--- backend/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import *
-
-class StudentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Student
-        fields = ('id', 'first_name', 'last_name', 'email', 'phone_number', 'city', 'comment')
