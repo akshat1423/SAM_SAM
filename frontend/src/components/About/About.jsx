b05aade1aaa3ce69fe5edf106a2677f12410c5d4
KhushiYadav18 <yadavkhushi00045@gmail.com>
2024-07-09 17:21:48 +0530

    adding information

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -26,24 +26,20 @@ export default function About() {
             </div>
             <div className="aboutContent">
                 <p>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam dolorem quaerat cupiditate,
-                    assumenda distinctio architecto iure, placeat, impedit sed expedita fugit a accusantium labore
-                    minus repellat optio perferendis asperiores amet? Temporibus assumenda, ipsum molestias ratione
-                    possimus aut, perferendis ullam ipsam delectus sapiente dolore beatae earum rerum voluptas?
-                    Explicabo ad, minus, laborum quas sunt odio, quod rerum aut ex iste dicta? Neque corrupti modi
-                    perspiciatis nostrum dolorem aut laborum in dolorum omnis maxime laboriosam, doloremque, quis
-                    voluptatem fugiat. Consequatur facilis et, rem modi neque adipisci eaque, rerum saepe maiores non
-                    quis. Exercitationem deserunt voluptatem, eius cumque similique mollitia ut placeat sed libero
-                    eligendi id facere natus maiores voluptatum vel odit quod earum recusandae nobis non? Qui quod
-                    commodi atque minima dicta? Nesciunt aliquid unde ipsam omnis quo, dolorum vel odio inventore sunt
-                    asperiores, maiores quasi facere nemo ut culpa placeat autem officiis eligendi, dolor dolore
-                    facilis corrupti voluptas similique! Ex, accusamus. Optio, molestiae ipsa quasi esse, quidem ad
-                    eveniet dolorem odit odio officia deserunt porro nostrum maiores assumenda. Quidem officiis
-                    eligendi obcaecati autem itaque voluptatum dignissimos, et, nam, incidunt ex dolorem. Itaque
-                    deserunt est quibusdam mollitia dicta saepe, sit dolorum velit magni doloremque perspiciatis alias
-                    incidunt architecto autem illo vel dolores! Fugit saepe sit quis quos placeat error aliquam hic
-                    unde! Quidem labore ipsa qui fugit veniam adipisci vel praesentium ullam, fugiat error, odit
-                    dolorem repudiandae tempora consequuntur ad doloribus omnis neque dolor dolorum perspiciatis eum.
+                The Student Alumni Meet in the city is aimed to be a welcome event for
+                    successful candidates who will be joining IITs and for current IITB students. It is
+                    an opportunity for them to get guidance from the alumni of IIT Bombay. It will be
+                    an event where they will get to know ‘what life looks like after getting out of IIT
+                    and also what is the current scenario of life in the institute’ from the experiences
+                    of our esteemed alumni and the current students (our team), respectively.
+                    Structure:
+                    1. Speech by an alumnus (telling about the life after graduating from the
+                    institute with a broader perspective i.e. not specific to a particular field)
+                    2. Presentation by the Chapter (in case of IITB, we have alumni chapters),
+                    briefing about the activities done round the year (the invited students as
+                    well as the current students will get to know about these)
+                    3. Common Q&A session where the doubts of students and their parents will
+                    be addressed.
                 </p>
             </div>
         </div>
