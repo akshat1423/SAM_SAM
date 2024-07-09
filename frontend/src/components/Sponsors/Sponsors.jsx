903251d42b0681158cccbe452659341c644636b4
Akshat Jain <125379408+akshat1423@users.noreply.github.com>
2024-07-09 15:40:17 +0530

    Update Sponsors.jsx

--- frontend/src/components/Sponsors/Sponsors.jsx
+++ frontend/src/components/Sponsors/Sponsors.jsx
@@ -24,16 +24,6 @@ const Sponsors = () => {
   return (
     <div className='sponsors-wrapper'>
     <h2 className='sponsors-heading'>OUR SPONSORS</h2>
-    <div className="sponsors-container">
-        
-{/*       {sponsors.map((sponsor, index) => (
-        <Tilt className="tilt" options={tiltOptions} key={index}>
-          <div className="sponsor-logo">
-            <img src={sponsor.logo} alt={sponsor.name} />
-          </div>
-        </Tilt>
-      ))} */}
-    </div>
     </div>
   );
 };
